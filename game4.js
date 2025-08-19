74a84e3a5b7c3b07eef3d37217a9d26e2815be42
Vyacheslavlinux <vpetrov@engineer.com>
2025-08-19 12:59:00 +0400

    1

--- game4.js
+++ game4.js
@@ -27,7 +27,7 @@ for (let index = 0; index < quiz.length; index++) {
       } 
       if (x === Number(1) || x === Number(2) || x === Number(3)) {} 
       if (x === Number(null)){
-         exit;
+        break;
       }  
     }
 let text = "Количество правильных ответов " + correctAnswerUser;
